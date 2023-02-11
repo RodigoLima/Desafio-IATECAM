f47457f859f6850cda9c6853228b5b55dcc18161
Rodrigo <ferreirarodrigo230@gmail.com>
2023-02-11 07:05:38 -0400

    criacao dos schema

--- backend/schemas/Product_schema.py
+++ backend/schemas/Product_schema.py
@@ -5,7 +5,9 @@ from pydantic import BaseModel as SCBaseMode
 class CategorySchema(SCBaseMode):
     id: Optional[int]
     name:str
-    
+    category: int
+    price: float
+    serie: int
     
     class config:
         orm_mode = True
