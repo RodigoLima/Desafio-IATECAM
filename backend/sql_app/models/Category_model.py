9d1d716a4468ddba8cb09c80bd06a2e6b9fea2aa
Rodrigo <ferreirarodrigo230@gmail.com>
2023-02-11 16:56:56 -0400

    revisao da category_model e adicionado comentarios

--- backend/sql_app/models/Category_model.py
+++ backend/sql_app/models/Category_model.py
@@ -1,11 +1,20 @@
+# Importa as classes necessárias para definir colunas em uma tabela de banco de dados
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
+# Importa a classe Base da biblioteca 'database'
 from core.database import Base
-from sqlalchemy import Column, Integer, String,ForeignKey,Float
 
 class Category(Base):
+    """
+    Classe que representa a tabela 'category' no banco de dados. Cada instância dessa classe 
+    representa uma linha na tabela que armazena informações sobre uma categoria dos produtos.
+    """
+    
+    # Nome da tabela no banco de dados
     __tablename__ = "category"
     
-    id: int = Column(Integer, primary_key=True,autoincrement=True)  
-    name: str = Column(String(128),nullable=False)  
+    # Coluna 'id' da tabela
+    id = Column(Integer, primary_key=True, autoincrement=True, doc="Id da categoria")
     
-
+    # Coluna 'name' da tabela
+    name = Column(String(128), nullable=False, doc="Nome da categoria")
